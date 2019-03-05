651d8c5084264bda1f71b299bdf0d8b71286d570
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-03-05 19:51:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ function iLoveTheBeatles(number){
   var array = [];
   do {
     array.push("I love the Beatles!");
-  } while (iLoveTheBeatles() < 15);
+    number++
+  } while (number < 15);
+  return array
 }
