24354715ad854652b6a224aa34cdb9d9ff50200e
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-03-05 19:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,2 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments){ // add solution here
-  var beatles = []
-  for (var i=0; i < instruments.length; i++ ){
-    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
-  }
-  return beatles
-}
 
-function johnLennonFacts(facts){
-  var beat = []
-  for (var i=0; i < facts.length; i++){
-    beat.push(`{facts[i]}!!!`)
-  }
-  return beat
-while(facts>0){
-  console.log(facts++)
-}
-return facts
-}
