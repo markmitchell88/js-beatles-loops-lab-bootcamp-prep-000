6179cf35b56442765f220f172e07f0ec9360a13f
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-03-05 19:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var beatles = [];
-  for (let i = 0, i < musicians.length; i++)
+  for (let i = 0; i < musicians.length; i++)
   {
     var string = `${musicians[i]} plays ${instruments[i]}`;
     beatles.push(string);
