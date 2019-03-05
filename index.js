6621a92fe261f83b5a100f9a42c681f2c7b162ec
Mark Mitchell <mmitchell88@hotmail.co.uk>
2019-03-05 19:08:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){ // add solution here
+  var beatles = []
+  for (var i=0; i < instruments.length; i++ ){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatles
+}
+
+function johnLennonFacts(facts){
+  var beat = []
+  for (var i=0; i < facts.length; i++){
+    beat.push(`{facts[i]}!!!`)
+  }
+  return beat
+while(facts>0){
+  console.log(facts++)
+}
+return facts
+}
